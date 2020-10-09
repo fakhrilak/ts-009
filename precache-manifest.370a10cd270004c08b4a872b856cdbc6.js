aea78963a0d6db863005e7fe1e406cd4563f9131
fakhrilak <fakhrilak23@gmail.com>
2020-10-09 13:17:36 +0700

    Updates

---
+++ precache-manifest.370a10cd270004c08b4a872b856cdbc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc25356ac2684f22ebe67fd06a3ef57",
+    "url": "/ts-009/index.html"
+  },
+  {
+    "revision": "f2a8c99d575fc1a303ff",
+    "url": "/ts-009/static/css/main.e99ea75c.chunk.css"
+  },
+  {
+    "revision": "8e3fb8a164505189f83d",
+    "url": "/ts-009/static/js/2.433cc17d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ts-009/static/js/2.433cc17d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a8c99d575fc1a303ff",
+    "url": "/ts-009/static/js/main.a1504362.chunk.js"
+  },
+  {
+    "revision": "55b0b67ffdaa38c5730b",
+    "url": "/ts-009/static/js/runtime-main.fa3b336a.js"
+  },
+  {
+    "revision": "7bfeb01c4de57d5f688936ab5672ae62",
+    "url": "/ts-009/static/media/logo.7bfeb01c.png"
+  }
+]);
